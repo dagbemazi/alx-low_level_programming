35d0ffe71a7e565a7c2711754a512486ceb8ad8b
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-14 00:52:12 +0000

    script: size of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Print size of various data types
+  *
+  * Return: Always 0(success)
+  */
+
+
+int main(void)
+{
+	int charSize = sizeof(char);
+	int intSize = sizeof(int);
+	int longIntSize = sizeof(long int);
+	int longLongIntSize = sizeof(long long int);
+	int floatSize = sizeof(float);
+
+	printf("Size of a char: %d bytes(s)\n", charSize);
+	printf("Size of a int: %d bytes(s)\n", intSize);
+	printf("Size of a long int: %d byte(s)\n", longIntSize);
+	printf("Size of a long long int: %d byte(s)\n", longLongIntSize);
+	printf("Size of a float: %d byte(s)\n", floatSize);
+
+	return (0);
+}
