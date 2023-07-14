8f24b45edb27071363a52f0dc4db93702b441aa4
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-14 00:57:45 +0000

    fix: remove trailing s

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,8 +16,8 @@ int main(void)
 	int longLongIntSize = sizeof(long long int);
 	int floatSize = sizeof(float);
 
-	printf("Size of a char: %d bytes(s)\n", charSize);
-	printf("Size of a int: %d bytes(s)\n", intSize);
+	printf("Size of a char: %d byte(s)\n", charSize);
+	printf("Size of a int: %d byte(s)\n", intSize);
 	printf("Size of a long int: %d byte(s)\n", longIntSize);
 	printf("Size of a long long int: %d byte(s)\n", longLongIntSize);
 	printf("Size of a float: %d byte(s)\n", floatSize);
