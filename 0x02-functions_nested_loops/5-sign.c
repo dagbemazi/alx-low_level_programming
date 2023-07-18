aaeaf607d990c3aebf2afad5df99c13a0e96d337
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-18 23:11:53 +0000

    sign of integer

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * print_sign - check sign of number
+  * @n: character to check against
+  * Return: 1 if True, 0 if false
+  */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+");
+		return (1);
+	} else if (n == 0)
+	{
+		printf("0");
+		return (0);
+	return (-1);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -19,4 +19,5 @@ void print_alphabet(void);
 void print_to_98(int n);
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
 #endif
