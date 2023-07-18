991456f3dcef7947ea4de5cf27e4781cbfb46c22
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-18 23:23:06 +0000

    position

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,15 +10,13 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
 		return (1);
+		putchar('+');
 	} else if (n == 0)
 	{
-		putchar('0');
 		return (0);
-	} else
-	{
-		putchar('-');
-		return (-1);
+		putchar('0');
 	}
+	return (-1);
+	putchar('-');
 }
