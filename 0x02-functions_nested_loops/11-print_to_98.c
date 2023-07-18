de5b772592cea4bfd998cd57595c8e0a462f1791
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-18 22:32:49 +0000

    fix last number

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,5 @@
 #include <stdio.h>
 
-/**
-  * main - entry point
-  * Return: always (0) success
-  */
-int main(void)
-{
-	print_to_98(100);
-	print_to_98(0);
-	print_to_98(98);
-	return (0);
-}
 
 /**
   * print_to_98 - Print numbers up to 98
@@ -30,16 +19,39 @@ void print_to_98(int n)
 		{
 			for (n = 0; n <= 98; n++)
 			{
-				printf("%d, ", n);
+				if (n != 98)
+				{
+					printf("%d, ", n);
+				} else
+				{
+					printf("%d", n);
+				}
 			}
 		} else if (n > 98)
 		{
 			for (n = n; n >= 98; n--)
 			{
-				printf("%d, ", n);
+				if (n != 98)
+				{
+					printf("%d, ", n);
+				} else
+				{
+					printf("%d", n);
+				}
 			}
 		}
 	}
 	printf("\n");
 }
 
+/**
+  * main - entry point
+  * Return: always (0) success
+  */
+int main(void)
+{
+	print_to_98(100);
+	print_to_98(0);
+	print_to_98(98);
+	return (0);
+}
