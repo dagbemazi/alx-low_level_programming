48bc2e6d2710f33da7d906b4e626ce27755d80d2
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-18 17:11:27 +0000

    fix: prototype declare

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	_putchar('_putchar\n');
+	_putchar();
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,19 @@
-void _putchar();
+#include <stdio.h>
+
+/**
+  * _putchar - prototype for putchar()
+  *
+  * Return: void
+  */
+
+void _putchar()
+{
+	char text[] = "_putchar";
+	int i;
+
+	for (i=0; text[i] != '\0'; i++)
+	{
+		putchar(text[i]);
+	}
+	putchar(10);
+}
