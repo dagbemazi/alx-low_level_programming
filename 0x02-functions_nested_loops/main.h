8168c87141414c3040db0e94ccf15b0282917da5
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-18 22:41:00 +0000

    fix: include prototype in main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,9 +9,12 @@
   * _putchar - requires char int value
   * print_alphabet - proto
   * Return: void
+  * print_to_98 - print numbers to 98
+  * @n - number to print from
   */
+
 
 int _putchar(char c);
 void print_alphabet(void);
-
+void print_to_98(int n);
 #endif
