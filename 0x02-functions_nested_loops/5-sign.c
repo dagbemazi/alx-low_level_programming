3aef8ea766719239ac8ad106e3807518b9426e8c
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-19 00:13:35 +0000

    fix: putchrr

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,18 +10,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		_putchar('+');
 		return (1);
 	} else if (n == 0)
 	{
-		printf("0");
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		printf("-");
+		_putchar('-');
 		return (-1);
 	}
 
-	printf("\n");
+	_putchar('\n');
 }
