a3f4a03acbe03086d67fa7db81a1d7c74ff3c9a7
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-19 03:43:19 +0000

    solve 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,6 +13,9 @@ int print_last_digit(int k)
 	int last_digit;
 
 	last_digit = k % 10;
-	_putchar('0' + last_digit);
+	
+	if (k < 0)
+		last_digit = -last_digit;
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
