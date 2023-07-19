c8fb0a9bc38143dcc08f7731ca555b54157f6414
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-19 02:06:19 +0000

    9x table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+  * times_table - print 9 times table
+  *
+  * Return: void
+  */
+
+void times_table(void)
+{
+	int loop, multiple, multiplier;
+
+	multiplier = 9;
+
+	for (loop = 0; loop < multiplier; loop++)
+	{
+		for (multiple = 0; multiple <= 9; multiple++)
+		{
+			int product;
+
+			product = loop * multiple;
+
+			if (multiple != multiplier)
+			{
+				printf("%d, ", product);
+			}
+			else
+			{
+				printf("%d", product);
+			}
+		}
+		printf("\n");
+	}
+}
