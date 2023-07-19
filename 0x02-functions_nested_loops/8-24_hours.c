615c1f2aa841c3af0ce74b8eefad39dde4c6ee9d
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-19 03:54:49 +0000

    time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
   * jack_bauer - print minutes of day starting
   * from 00:00 to 23:59
@@ -18,19 +19,10 @@ void jack_bauer(void)
 		{
 			first_hr = i / 10;
 			second_hr = i % 10;
+			first_min = j / 10;
+			second_min = j % 10;
 
-			first_min = i / 10;
-			second_min = i % 10;
-
-			printf("%d%d:%d%d\n", first_hr, second_hr, first_min,
-					second_min);
-
+			printf("%d%d:%d%d\n", first_hr, second_hr, first_min,second_min);
 		}
 	}
-}
-
-int main(void)
-{
-	jack_bauer();
-	return (0);
 }
