834311999ca14395a810b3b66729cf4bed2384d2
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-19 00:39:57 +0000

    last digit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
   * print_sign - check sign of number
   * @n: character to check against
@@ -9,7 +9,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('43');
+		printf("+, ");
 		return (1);
 	} else if (n == 0)
 	{
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_last_digit - returns the last digit of a number
+  *
+  * @k: number to pick last digit from
+  *
+  * Return: last digit
+  */
+
+int print_last_digit(int k)
+{
+	int last_digit;
+
+	last_digit = k % 10;
+	return (last_digit);
+}
