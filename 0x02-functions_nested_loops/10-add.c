9b2c2f15c4c3ef0ed09b82486078aa71b83d380a
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-19 02:16:29 +0000

    remove extra chars

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+/**
+  * add - add two values
+  * @a @b: values to add
+  * Return: sum of values
+  */
+
+
+
+
+
+
+
+int main(void)
+{
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
