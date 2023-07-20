e691e49c22fdc80a563a4d2a7725f0640b3609f4
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-20 23:34:36 +0000

    multiply and collaborate

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/**
+  * mul - program to multiply two integers
+  * @a: first integer
+  * @b: 2nd integer
+  * Return: Product of integers
+  */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
