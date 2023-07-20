85f6abc9c789a8869659adcb81a9233a1fbdc020
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-20 16:17:37 +0000

    checks 8?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,11 +16,11 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (a > b && a > c)
+	else if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
