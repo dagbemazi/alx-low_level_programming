cc0037277ba44d6a427839ab307778b4b296d0d2
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-21 01:36:48 +0000

    5 more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * more_numbers - print 0 - 14 to 10 times
+  * Return: void
+  */
+
+void more_numbers(void)
+{
+	int range, i;
+
+	for (range = 0; range < 10; range++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar(49);
+				_putchar((i % 10) + '0');
+			}
+			else
+			{
+				_putchar(i + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
