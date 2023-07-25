1af402201bbd002676173509e7e4c470c6f650a1
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-25 01:32:16 +0000

    rev the engine

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,41 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
-  * print_rev - reverse a string
-  * @s: string input
+  * str_len - find the length of the string
+  * @text: string to find length for
+  * Return: length of string
+  */
+
+int str_len(char *text)
+{
+	int length;
+
+	length = 0;
+
+	while (text[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
+
+
+/**
+  * print_rev - print string in reverse followed by newline
+  * @s: string to reverse
+  * Return: void
   */
 
 void print_rev(char *s)
 {
-	printf("%s\n", s);
+	int length, a;
+
+	length = str_len(s);
+
+	for (a = length - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
 }
