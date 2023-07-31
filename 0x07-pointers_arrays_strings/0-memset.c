ed9496f20a1e06b20a9773820f5d907ca5dd5013
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-31 23:56:27 +0000

    fix: betty style

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,12 +3,14 @@
   * @s: pointer of buffer to fill
   * @b: bytes
   * @n: number of bytes to fill
+  * Return: pointer to buffer.
   */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-    for (int i = 0; i < n; i++)
-        *(s+i) = b;
-
-    return (s);
+	for (int i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
 }
