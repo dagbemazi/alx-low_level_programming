0139ad7e585faeae79f8e4852ffe87a50d084826
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-31 23:51:18 +0000

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+/**
+  * _memset - fills memory with constant bytes
+  * @s: pointer of buffer to fill
+  * @b: bytes
+  * @n: number of bytes to fill
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+    for (int i = 0; i < n; i++)
+        *(s+i) = b;
+
+    return (s);
+}
