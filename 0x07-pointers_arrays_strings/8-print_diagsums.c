8a2bcc7b541037e9183a46267558124d6bb87af2
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-07-31 23:34:18 +0000

    updates

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * print_diagsums - print the sum of matrix diagonals.
+  * @a: pointer to multidimensional array casted as int.
+  * @size: size of array
+  * Return: Nothing.
+  */
+
+void print_diagsums(int *a, int size)
+{
+
+	
+}
