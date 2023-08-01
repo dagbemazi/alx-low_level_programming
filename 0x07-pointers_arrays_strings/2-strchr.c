a702c0e39debd485b42315a245de7e1c707d9401
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-08-01 01:56:17 +0000

    ftrchr'

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,20 @@
 /**
+  * _strlen - find length of string
+  * @s: input string
+  * Return: length
+  */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+
+	return (length);
+}
+
+
+/**
   * _strchr - locates a character in string
   * @s: pointer to string
   * @c: character to search
@@ -8,4 +24,25 @@
 
 char *_strchr(char *s, char c)
 {
-	
+	int len, *p;
+
+	len = _strlen(s);
+
+	for (i = 0; i < len && s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			p = &(s[i]);
+			break;
+		}
+	}
+
+	if (p)
+	{
+		return (p);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
