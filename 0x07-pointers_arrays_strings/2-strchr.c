54a17d91b13f7ef7cb8d29c5a8b4aa931b763aa4
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-08-01 01:12:22 +0000

    fix: braces

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,11 @@
+/**
+  * _strchr - locates a character in string
+  * @s: pointer to string
+  * @c: character to search
+  * Return: pointer first occurence of @c,
+  * NULL if not found
+  */
+
+char *_strchr(char *s, char c)
+{
+	
