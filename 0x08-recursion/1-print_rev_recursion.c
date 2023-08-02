d3a1bf923c6b1d1e7fe033b623437846fc3499d3
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-08-02 23:29:18 +0000

    reverse str

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * _print_rev_recursion - recursively prints a character.
+  * @s: pointer to input
+  * Return: Nothing.
+  */
+
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		i++;
+		_puts_recursion(&s[i]);
+		_putchar(s[i]);
+	}
+}
