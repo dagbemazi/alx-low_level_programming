2dc7084db900d31abcb5cccc71a110fec86b40a8
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-08-03 23:26:14 +0000

    coin changes

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
   * main - entry point.
-  * print the name of file
+  * print the minimum number of coins to make change
+  * for an amount of money.
   * @argc: argument counter
   * @argv: pointer to argument variables
   * Return: 0 always on success.
@@ -10,5 +12,45 @@
 
 int main(int argc, char *argv[])
 {
+	int sum;
 
+	sum = 0;
+
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int cents, remainder1, remainder2;
+		int remainder3, remainder4;
+
+		cents = atoi(argv[1]);
+
+		if (cents < 0)
+		{
+			printf("%d\n", 0);
+		}
+		else
+		{
+			sum += cents / 25;
+			remainder1 = cents % 25;
+
+			sum += remainder1 / 10;
+			remainder2 = remainder1 % 10;
+
+			sum += remainder2 / 5;
+			remainder3 = remainder2 % 5;
+
+			sum += remainder3 / 2;
+			remainder4 = remainder3 % 2;
+
+			sum += remainder4;
+
+			printf("%d\n", sum);
+		}
+	}
+
+	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
   * main - entry point.
-  * print the name of file
+  * print the sum of infinite integer args.
   * @argc: argument counter
   * @argv: pointer to argument variables
   * Return: 0 always on success.
@@ -10,5 +11,31 @@
 
 int main(int argc, char *argv[])
 {
+	int i, sum;
 
+	sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!atoi(argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum += atoi(argv[i]);
+			}
+		}
+
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("%d\n", 0);
+	}
+
+	return (0);
 }
