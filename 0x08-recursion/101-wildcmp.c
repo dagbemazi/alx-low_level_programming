2ab406b21fc60eacbc11fd4c64df9fdcfb314e60
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-08-03 03:15:51 +0000

    pointer comparison

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,9 +7,9 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (s[i] == '\0')
+	if (*s == '\0')
 	{
-		if (s2 != '\0' && s2 == "")
+		if (*s2 != '\0' && s2 == "")
 		{
 			return (wildcmp(s1, s2 + 1));
 		}
@@ -19,7 +19,7 @@ int wildcmp(char *s1, char *s2)
 		}
 	}
 
-	if (s2 == "")
+	if (*s2 == "")
 	{
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
