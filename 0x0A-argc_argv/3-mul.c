f65f1107ea269c8be3f2c9afc0c7a9723f881233
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-08-03 15:58:43 +0000

    multiple two

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
   * main - entry point.
-  * print the name of file
+  * print the product of two numbers.
   * @argc: argument counter
   * @argv: pointer to argument variables
   * Return: 0 always on success.
@@ -10,5 +11,19 @@
 
 int main(int argc, char *argv[])
 {
+	int product;
 
+	if (argc > 1)
+	{
+		product = atoi(argv[1]) * atoi(argv[2]);
+
+		printf("%d\n", product);
+
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 }
