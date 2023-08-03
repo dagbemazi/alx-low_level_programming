b3ab387094266e12ac8e676591115464d7b966c7
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-08-03 01:26:03 +0000

    fix: no newline

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,16 +8,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] == '\0')
+	if (*s)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		i++;
-		_print_rev_recursion(&s[i]);
-		_putchar(s[i]);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
