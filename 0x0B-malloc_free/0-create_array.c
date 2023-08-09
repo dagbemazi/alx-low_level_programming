8b4755580c5406de2ce6e8437c883e1d03a143d0
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-08-09 00:34:43 +0000

    mem alloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+/**
+  * create_array - allocates an array of chars,
+  * initialize array with specific char.
+  * @size: size of array
+  * @c: char to initialize with.
+  * Return: Null if memory not allocated,
+  *         else pointer to array.
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+	unsigned int memsize = sizeof(c) * size;
+
+	if (size == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		ptr = malloc(memsize);
+
+		for (i = 0; i < memsize; i++)
+		{
+			ptr[i] = c;
+		}
+	}
+
+	return (ptr);
+}
