4f21eabf53b537d812b5dc1da4ccbabe63731478
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-08-15 01:47:46 +0000

    fix: document struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+  * struct dog - a struct for dog features
+  * @name: name of dog
+  * @age: dog age
+  * @owner: owner of dog
+  */
+
 struct dog
 {
 	char *name;
