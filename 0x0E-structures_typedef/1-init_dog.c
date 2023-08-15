9ca1681557e55cc8c28a1aad14ca6050f9d4fae3
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-08-15 01:54:37 +0000

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+/**
+  * init_dog - initialize type for struct dog
+  * @d: pointer to struct
+  * @name: pointer to dog name
+  * @age: age of dog
+  * @owner: pointer to owner name
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
