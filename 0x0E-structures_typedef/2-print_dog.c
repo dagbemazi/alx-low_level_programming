023f1a2abeb817beddadaf0cd83dcaa17f712f4b
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-10-10 01:17:01 +0000

    2 dogs

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+  * print_dog -prints the struct dog
+  * @d: pointer to struct dog
+  * Return: nothing
+  */
+
+void print_dog(struct dog *d)
+{
+	char *name = (*d).name;
+	char *owner = (*d).owner;
+	float age = (*d).age;
+
+	if (d == NULL)
+	{
+		return;
+	}
+
+	if (name == NULL)
+		name = "(nil)";
+
+	if (owner == NULL)
+		owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", name, age, owner);
+
+}
