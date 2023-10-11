36f2c9cfd041f8563755a9cd128e117ef089682a
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-10-11 23:21:13 +0000

    fix: args documentation, null pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,16 @@
+#include <stdlib.h>
 
 /**
   * print_name - function pointer to print a name
-  * name: string name to print
-  * f: pointer to function to call
+  * @name: string name to print.
+  * @f: pointer to function to call.
   * Return: nothing
   */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		return;
+
 	f(name);
 }
