894f89b417bb46bf8e756e6409210f1b3592e2f0
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-10-11 23:59:38 +0000

    fix: type order

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,9 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i;
+	unsigned long int i;
 
-	if (action == NULL)
+	if (array == NULL || action == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
