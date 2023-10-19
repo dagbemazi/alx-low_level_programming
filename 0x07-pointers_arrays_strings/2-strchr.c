078bd1921e091d2499b79f60c1e83ad75d77a6f6
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-10-19 00:32:48 +0000

    fix: point to string end

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,5 @@
+int _strlen(char *s);
+
 /**
   * _strchr - locates a character in string
   * @s: pointer to string
@@ -7,7 +9,11 @@
 
 char *_strchr(char *s, char c)
 {
-	while (s)
+	/* Return pointer to end of string */
+	if (c == '\0')
+		return (s + _strlen(s));
+
+	while (*s)
 	{
 		if (*s == c)
 			return (s);
@@ -16,4 +22,25 @@ char *_strchr(char *s, char c)
 	}
 
 	return (0);
+}
+
+
+/**
+  * _strlen - find length of string
+  * @s: pointer to input string
+  * Return: length of string
+  */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
 }
