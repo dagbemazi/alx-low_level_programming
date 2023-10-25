8e156a33943ad1d24fe77af1785154bf51728ee9
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-10-25 23:45:03 +0000

    feat: Find sum of list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+  * sum_listint - compute sum of all data(n) of a list
+  * @head: pointer to head
+  * Return: 0 if list is empty else sum 
+  */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
