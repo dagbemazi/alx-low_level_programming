6813f6cb96113ecbbd161ebc0f63e3bd6af71125
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-10-26 02:11:06 +0000

    chore: data should not be negative

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp;
 	listint_t *new_node = malloc(sizeof(listint_t));
 
-	if (new_node == NULL || head == NULL || *head == NULL || idx)
+	if (head == NULL || *head == NULL || new_node == NULL || n < 0 || idx)
 		return (NULL);
 
 	new_node->n = n;
