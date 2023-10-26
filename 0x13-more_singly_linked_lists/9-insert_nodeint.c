11793f63c294d22ae2db37d27234776abbe41d2c
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-10-26 01:59:09 +0000

    feat: add node at nth position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * insert_nodeint_at_index - insert node at nth position
+  * @head: pointer to node
+  * @idx: position to insert, index starts at 0
+  * @n: data to insert
+  * Return: address of new node or NULL on failure
+  */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *temp;
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL || head == NULL || *head == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (idx == 1)
+	{
+		new_node->next = *head;
+		*head = new_node;
+
+		return (new_node);
+	}
+
+	temp = *head;
+
+	for (i = 0; i < idx - 2; i++)
+		temp = temp->next;
+
+	new_node->next = temp->next;
+	temp->next = new_node;
+
+	return (new_node);
+}
