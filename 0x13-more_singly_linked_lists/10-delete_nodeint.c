85edf364a8ec9f1e743c54a251f8e2e9f3a50bb3
Dickson Agbemazi <63107056+dagbemazi@users.noreply.github.com>
2023-10-26 02:43:21 +0000

    feat: delete nth node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+  * delete_nodeint_at_index - delete node
+  * @head: pointer to head
+  * @index: node to be deleted
+  * Return: 1 on success, -1 for failure
+  */
+
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *store_node, *temp;
+	unsigned int i = 0;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	store_node = *head;
+
+	if (index == i)
+	{
+		*head = store_node->next;
+		free(store_node);
+
+		return (1);
+	}
+
+	for (i = 0; i < index - 1; i++)
+		store_node = store_node->next;
+
+	temp = store_node->next;
+	store_node->next = temp->next;
+	free(temp);
+
+	return (1);
+}
